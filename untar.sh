5102a9bbaddd5473298b84967b1610061127eaca
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2020-05-12 13:14:06 +0200

    change posix safe tar syntax add more packer

--- untar.sh
+++ untar.sh
@@ -9,9 +9,14 @@
 untar() {
   tarball=$1
   case "${tarball}" in
-    *.tar.gz | *.tgz) tar -xzf "${tarball}" ;;
-    *.tar) tar -xf "${tarball}" ;;
+    *.tar.gz | *.tgz) TAR_CMD xzf "${tarball}" ;;
+    *.tar.xz | *.txz) xz -cd "${tarball}" | tar xf - 2>&1  ;;
+    *.tar.Z | *.tar.z | *.taz) uncompress -c "${tarball}" | tar xf - 2>&1 || gzip -cd "${tarball}" | tar xf - 2>&1 ;;
+    *.tar.bz2 | *.tar.bz | *.tbz2 | *.tbz | *tar.bzip2) bzip2 -cd "${tarball}" | tar xf - 2>&1 ;;
+    *.tar) tar xf "${tarball}" ;;
     *.zip) unzip "${tarball}" ;;
+    *.7z) 7z x "${tarball}" || 7za x "${tarball}" ;;
+    *.rar) unrar "${tarball}" ;;
     *)
       log_err "untar unknown archive format for ${tarball}"
       return 1
